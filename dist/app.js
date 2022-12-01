47707a4373c38ec10a8b115a809c597f1e7a7268
hussain2023 <iamhussainali2@gmail.com>
2022-12-01 09:51:03 +0530

    data type and union

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,20 @@
 "use strict";
 var message = "Hellow Typescript from message";
+var message2 = "new message from message";
+var message3 = "new message from message3";
 console.log(message);
+console.log(message2);
+console.log(message3);
+// console.log(language.length);
+// let myName: string = "Raju";
+// myName = "Sakil";
+// let age: number;
+// age = 'arhdo'
+// age = 45
+// Union type
+var age = 56;
+age = "89";
+console.log(typeof age);
+// const type
+var friendName = "Jafar";
+var friendName2 = "Jafar";
--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,28 @@
 const message = "Hellow Typescript from message";
+const message2 = "new message from message";
+const message3 = "new message from message3";
+
 console.log(message);
+console.log(message2);
+console.log(message3);
+// console.log(language.length);
+
+// let myName: string = "Raju";
+// myName = "Sakil";
+
+// let age: number;
+// age = 'arhdo'
+// age = 45
+
+// Union type
+
+let age: number | string = 56;
+
+age = "89";
+
+console.log(typeof age);
+
+// const type
+
+const friendName: string = "Jafar";
+const friendName2: "Jafar" = "Jafar";
